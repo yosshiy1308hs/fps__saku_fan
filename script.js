56f5673ad5391308937f4310af5ce68272ec9793
saltex <saltex112384ex@gmail.com>
2025-05-01 19:10:07 +0900

    modified:   script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const supabaseUrl = 'https://uiguxlwkoaclvugqwomw.supabase.co';
-const supabaseKey = 'your-anon-key';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZ3V4bHdrb2FjbHZ1Z3F3b213Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4MzM1NTAsImV4cCI6MjA2MTQwOTU1MH0.0Uz4BbXvOWCx8EHxN6whml3GprdYLeTpVevB6pM3fBk';
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
 let heartCount = 0;
