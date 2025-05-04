ee4538f3fb47e82715be222035311cb1c422f487
saltex <saltex112384ex@gmail.com>
2025-05-04 18:08:25 +0900

    modified:   script.js modified:   supabaseClient.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const supabaseUrl = 'https://uiguxlwkoaclvugqwomw.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZ3V4bHdrb2FjbHZ1Z3F3b213Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4MzM1NTAsImV4cCI6MjA2MTQwOTU1MH0.0Uz4BbXvOWCx8EHxN6whml3GprdYLeTpVevB6pM3fBk';
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+import { supabaseClient } from './supabaseClient.js';
 
 let heartCount = 0;
 let starCount = 0;
--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
 export const supabaseClient = supabase.createClient(
     'https://uiguxlwkoaclvugqwomw.supabase.co',
-    'your-anon-key'
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZ3V4bHdrb2FjbHZ1Z3F3b213Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4MzM1NTAsImV4cCI6MjA2MTQwOTU1MH0.0Uz4BbXvOWCx8EHxN6whml3GprdYLeTpVevB6pM3fBk'
 );
